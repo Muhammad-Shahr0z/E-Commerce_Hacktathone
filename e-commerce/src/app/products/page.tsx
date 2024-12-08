9caca32ff5f06941c8e8cf7a63fc1b9274763299
MuhammadShahroz <shahroxyt@gmail.com>
2024-12-09 00:28:25 +0500

    Update page.tsx

--- e-commerce/src/app/products/page.tsx
+++ e-commerce/src/app/products/page.tsx
@@ -87,7 +87,7 @@ const Products = () => {
 
 <FilterBar/>
 
-    <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center animate-cart-item">
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center">
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {products.map((product) => (
         <Card key={product.id} product={product} />
