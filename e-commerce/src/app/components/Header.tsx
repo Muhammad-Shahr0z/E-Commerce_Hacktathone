6cb98f07032db790c5c485c6963264ddec20b3c8
MuhammadShahroz <shahroxyt@gmail.com>
2024-12-18 23:01:17 +0500

    Update Header.tsx

--- e-commerce/src/app/components/Header.tsx
+++ e-commerce/src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { IoSearch } from "react-icons/io5";
@@ -18,15 +18,8 @@ import { useAtom } from "jotai";
 import { addToCart } from "../addToCart";
 import Navbar from "./Navbar";
 import { useEffect } from "react";
-import {
-  UserButton,
-  useUser
-} from '@clerk/nextjs'
+import { UserButton, useUser } from "@clerk/nextjs";
 import SignInButtonComponent from "./LoginButton";
-
-
-
-
 
 interface Product {
   image: string;
@@ -39,35 +32,23 @@ interface Params {
 }
 
 const Header = () => {
-  
   const { user, isLoaded, isSignedIn } = useUser();
   const [addCart, serAddToCart] = useAtom(addToCart);
-  useEffect(()=>{
-    
-  },[addCart.length])
-  
+  useEffect(() => {}, [addCart.length]);
 
   return (
     <>
-      <header className="max-w-[1440px] mx-auto md:h-[66px]  md:px-10 px-5 sticky top-0 md:static backdrop-blur-sm">
-
-
-
+      <header className="max-w-[1440px] mx-auto md:h-[66px] md:px-10 px-5 sticky top-0 md:static backdrop-blur-sm">
         <div className="md:border-b-[1px] border-[#0000004f] w-full mx-auto flex h-[66px] justify-between items-center md:pb-2">
           <IoSearch className="text-xl hidden md:block cursor-pointer" />
 
-          <h1 className="text-[#22202E] text-2xl font-semibold clashDisplay">
+          <h1 className="text-[#22202E] text-2xl font-semibold clashDisplay md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
             <Link href="/">Avion</Link>
           </h1>
 
           <div className="flex text-xl gap-[10px] items-center">
-
             <div className="hidden md:block">
-            {!isSignedIn ? (
-              <SignInButtonComponent/>
-            ) : (null)
-
-          }
+              {!isSignedIn ? <SignInButtonComponent /> : null}
             </div>
 
             <Link
@@ -76,51 +57,45 @@ const Header = () => {
             >
               <MdOutlineShoppingCart />
               <span className="flex justify-center items-center w-[20px] h-[20px] bg-red-500 mb-5 text-center rounded-full text-white font-semibold">
-  <span className="flex justify-center items-center text-[10px]">
-   {addCart.length}
-  </span>
-</span>
-
-
+                <span className="flex justify-center items-center text-[10px]">
+                  {addCart.length}
+                </span>
+              </span>
             </Link>
 
             <div className="hidden md:block">
               {!isSignedIn ? (
                 <CgProfile className="hidden md:block cursor-pointer " />
-              ): 
+              ) : (
                 <div className="flex justify-center items-center gap-2">
-                <UserButton/><span className="text-sm">{user?.fullName}</span>
+                  <UserButton />
+                  <span className="text-sm">{user?.fullName}</span>
                 </div>
-              
-              }
-            
+              )}
             </div>
 
-            <IoSearch className=" md:hidden cursor-pointer" />
+            <IoSearch className="md:hidden cursor-pointer" />
             <div className="md:hidden">
-
-            {!isSignedIn ? (
+              {!isSignedIn ? (
                 <CgProfile className="hidden md:block cursor-pointer " />
-              ): 
+              ) : (
                 <div className="flex justify-center items-center gap-2">
-                <UserButton/><span className="text-sm hidden">{user?.fullName}</span>
+                  <UserButton />
+                  <span className="text-sm hidden">{user?.fullName}</span>
                 </div>
-              
-              }
+              )}
             </div>
-            
-      
 
             <Link
               className="flex justify-center items-center md:hidden cursor-pointer"
               href="/carts"
-              >
+            >
               <MdOutlineShoppingCart />
               <span className="flex justify-center items-center w-[20px] h-[20px] bg-red-500 mb-5 text-center rounded-full text-white font-semibold">
-  <span className="flex justify-center items-center text-[10px]">
-  {addCart.length}
-  </span>
-</span>
+                <span className="flex justify-center items-center text-[10px]">
+                  {addCart.length}
+                </span>
+              </span>
             </Link>
             {/* Shadcn SHeet DIv */}
             <div className="md:hidden z-10 h-6 w-6">
@@ -131,13 +106,10 @@ const Header = () => {
                 <SheetContent>
                   <SheetHeader>
                     <SheetTitle className="text-4xl font-bold clashDisplay mb-4 text-center">
-                      
                       <Link href="/">Avion</Link>
-                      
                     </SheetTitle>
                     <SheetDescription>
                       <nav className="flex flex-col text-sm gap-y-3 border-[0.5px] items-center p-4 rounded-md">
-                        
                         <Link
                           href="/"
                           className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
@@ -188,21 +160,9 @@ const Header = () => {
                           Cutlery
                         </Link>
 
-     
-
                         <div className="md:hidden">
-            {!isSignedIn ? (
-              <SignInButtonComponent/>
-            ) : (null)
-
-          }
-            </div>
-
-     
-
-
-
-                        
+                          {!isSignedIn ? <SignInButtonComponent /> : null}
+                        </div>
                       </nav>
                     </SheetDescription>
                   </SheetHeader>
@@ -211,10 +171,9 @@ const Header = () => {
             </div>
           </div>
         </div>
-
       </header>
 
-    <  Navbar/>
+      <Navbar />
     </>
   );
 };
