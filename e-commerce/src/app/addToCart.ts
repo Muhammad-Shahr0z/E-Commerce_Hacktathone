365c08b4ef08b2221e6a776ddbd2c04cfb4092d4
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-03 21:55:19 +0500

    Update addToCart.ts

--- e-commerce/src/app/addToCart.ts
+++ e-commerce/src/app/addToCart.ts
@@ -1,34 +1,36 @@
 import { atom } from "jotai"
+import { atomWithStorage } from "jotai/utils"
 
 
-interface Product {
+interface ProductAddToCart {
   category: string;
   imageUrl: string;
   price: number;
   slug: string;
   name: string;
   Quantity: number;
-  Finalprice:number
+  Finalprice: number;
 }
 
 
-const initialCarts:Product[] = [
-    {
-      slug: "modern-cutlery-2",
-      category: "cutlery",
-      name: "The Lucky Lamp",
-      price: 299,
-      imageUrl: "/products/11.png",
-      Quantity: 1,
-      Finalprice:299
-    },
 
-  ]  
+// const initialCarts:ProductAddToCart[] = [
+//     {
+//       slug: "modern-cutlery-2",
+//       category: "cutlery",
+//       name: "The Lucky Lamp",
+//       price: 299,
+//       imageUrl: "/products/11.png",
+//       Quantity: 1,
+//       Finalprice:299
+//     },
+
+//   ]  
   
 
 
  
-  export const addToCart = atom(initialCarts);
+  export const addToCart = atomWithStorage<ProductAddToCart[]>('cartState', []);
   export const itemQuantity = atom(1)
   export const grandTotal = atom(0)
   export const totalCountAtom = atom(0)
