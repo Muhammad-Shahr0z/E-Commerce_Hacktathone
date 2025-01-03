40d8547d6f6dde7c19ec27c1d7270bfc25944a08
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-03 21:55:28 +0500

    Update page.tsx

--- e-commerce/src/app/studio/[[...tool]]/page.tsx
+++ e-commerce/src/app/studio/[[...tool]]/page.tsx
@@ -1,3 +1,5 @@
+
+
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -6,6 +8,7 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
+
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
