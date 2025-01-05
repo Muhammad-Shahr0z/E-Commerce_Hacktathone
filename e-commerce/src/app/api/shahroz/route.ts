cb8290e163fbfd4a03d85b163b3c8b1bf0c7ed37
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-05 23:17:11 +0500

    Update route.ts

--- e-commerce/src/app/api/shahroz/route.ts
+++ e-commerce/src/app/api/shahroz/route.ts
@@ -1,14 +1,22 @@
 import { client } from "@/sanity/lib/client";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-
-const fetchResponse = await client.fetch(`*[_type == "products"]{
-      name,category,price,
+export async function GET() {
+    // Sanity se data fetch karo
+    const fetchResponse = await client.fetch(`*[_type == "products"]{
+        name,
+        category,
+        price,
         "slug":slug.current,
         "imageUrl": image.asset->url
-  
-    }`)
-    return NextResponse.json(fetchResponse)
-}
+    }`);
 
+   
+    const responseWithName = {
+        createdBy: "Muhammad Shahroz", 
+        data: fetchResponse,         
+    };
+
+    // Response return karo
+    return NextResponse.json(responseWithName);
+}
