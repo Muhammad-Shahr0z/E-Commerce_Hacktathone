a1682263c5a58676a9f28355c34fd78d5e609c2c
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-27 04:01:00 +0500

    Deleted Static Images

--- e-commerce/interface.ts
+++ e-commerce/interface.ts
@@ -1,27 +1,25 @@
-interface Dimensions {
-    height: number;
+export interface Product {
+  imageUrl: string;
+  rating: {
+    count: number;
+    rate: number;
+  };
+  tags: string[];
+  price: number;
+  discount: number;
+  originalPrice: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+  stock: number;
+  dimensions: {
     depth: number;
     width: number;
-  }
-  
-  interface Rating {
-    rate: number;
-    count: number;
-  }
-  
- export interface Product {
-    tags: string[];
-    dimensions: Dimensions;
-    description: string;
-    slug: string;
-    rating: Rating;
-    originalPrice: number;
-    categoryName: string;
-    imageUrl: string;
-    name: string;
-    price: number;
-    stock: number;
-    id: number;
-    discount: number;
-  }
-  
+    height: number;
+  };
+  id: number;
+  description: string;
+  Quantity: number;
+  Finalprice: number;
+}
+
