a61f875afd865ced982765b0718d368774026ff2
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-27 13:45:36 +0500

    Update page.tsx

--- e-commerce/src/app/cancel/page.tsx
+++ e-commerce/src/app/cancel/page.tsx
@@ -16,7 +16,7 @@ export default function CancelPage() {
               Go to Home
             </a>
             <a
-              href="/cart"
+              href="/carts"
               className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
             >
               Try Again
