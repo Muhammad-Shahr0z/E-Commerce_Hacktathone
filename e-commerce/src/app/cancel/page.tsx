a6e9e789994f9d99f8e9950d100f3ca1e6232927
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-27 13:50:03 +0500

    Update page.tsx

--- e-commerce/src/app/cancel/page.tsx
+++ e-commerce/src/app/cancel/page.tsx
@@ -2,7 +2,7 @@
 
 export default function CancelPage() {
     return (
-      <div className="flex items-center justify-cente my-10">
+      <div className="flex items-center justify-center my-10">
         <div className="bg-white p-6 rounded-lg shadow-lg text-center">
           <h1 className="text-3xl font-bold text-red-600 mb-4">Payment Canceled</h1>
           <p className="text-gray-600 mb-6">
