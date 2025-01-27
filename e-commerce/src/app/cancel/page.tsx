d65a2a4b93f12ae11c431480d9f155c0486659a2
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-27 13:41:34 +0500

    Update page.tsx

--- e-commerce/src/app/cancel/page.tsx
+++ e-commerce/src/app/cancel/page.tsx
@@ -2,7 +2,7 @@
 
 export default function CancelPage() {
     return (
-      <div className="flex items-center justify-center bg-gray-100">
+      <div className="flex items-center justify-center bg-gray-100 my-10">
         <div className="bg-white p-6 rounded-lg shadow-lg text-center">
           <h1 className="text-3xl font-bold text-red-600 mb-4">Payment Canceled</h1>
           <p className="text-gray-600 mb-6">
@@ -16,7 +16,7 @@ export default function CancelPage() {
               Go to Home
             </a>
             <a
-              href="/checkout"
+              href="/cart"
               className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
             >
               Try Again
@@ -26,4 +26,7 @@ export default function CancelPage() {
       </div>
     );
   }
+  
+
+
   
