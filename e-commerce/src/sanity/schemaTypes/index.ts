8a7866ec1d1026ec8a451f08609739357a9ce43f
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-30 17:11:16 +0500

    Update index.ts

--- e-commerce/src/sanity/schemaTypes/index.ts
+++ e-commerce/src/sanity/schemaTypes/index.ts
@@ -1,9 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import {product} from "@/sanity/schemaTypes/product"
 import {category} from "@/sanity/schemaTypes/category"
-import customer from "@/sanity/schemaTypes/customer"
+import { customer } from './customer'
+import { order } from './order'
+import { orderItem } from './orderItem'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product,category,customer],
+  types: [product,category,orderItem,customer,order],
 }
