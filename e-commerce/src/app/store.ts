2b519cbef0788e41e723aa757969eb592ff0dc0c
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-30 17:11:29 +0500

    Update store.ts

--- e-commerce/src/app/store.ts
+++ e-commerce/src/app/store.ts
@@ -1,7 +1,26 @@
 import { atom } from "jotai";
-import { Product } from "../../interface";
+import { BillingDetails, Product } from "../../interface";
+import { DetailPreview } from "sanity";
+
+
+
+ const initialBillingDetails: BillingDetails = {
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    paymentMethod: "cashOnDelivery", 
+  }
+
 
 
 export const productsData = atom<Product[]|null>(null);
 export const filterCatogory = atom<string>("allProducts");
 export const inputValueAtom = atom<string>("");
+export const customerFormDetails = atom<BillingDetails>(initialBillingDetails);
+
+
+// Manage Loading State in Jotai for stripe payment
+export const isStripeLoading = atom<boolean>(false);
