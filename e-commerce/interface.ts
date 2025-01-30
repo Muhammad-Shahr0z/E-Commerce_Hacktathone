14fcd16b75416f46bd466e5bf4861e7a6db19e7d
MuhammadShahroz <shahroxyt@gmail.com>
2025-01-30 17:09:33 +0500

    Update interface.ts

--- e-commerce/interface.ts
+++ e-commerce/interface.ts
@@ -23,3 +23,37 @@ export interface Product {
   Finalprice: number;
 }
 
+export interface BillingDetails {
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  addressLine1: string;
+  addressLine2?: string; 
+  city: string;
+  paymentMethod: "cashOnDelivery" | "stripe"; 
+}
+
+
+
+export interface ProductDetail {
+  name: string;
+  imageUrl: string;
+  Finalprice: number;
+  Quantity: number;
+}
+
+export interface Billing{
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+}
+
+export interface Order {
+  customerDetails: Billing;
+  cartItems: ProductDetail[];
+  totalAmount: number;
+}
+
