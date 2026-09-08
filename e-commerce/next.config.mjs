ba068acaa32599a778d285f1be04c93a7a37f3c0
Muhammad Shahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2026-09-08 15:25:45 +0500

    Update next.config to resolve static generation issues

--- e-commerce/next.config.mjs
+++ e-commerce/next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+export default nextConfig;
